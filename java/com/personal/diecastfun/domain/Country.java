92031013bf5d2d2436ff12042321eab2f654c82f
Vincent Séguin <vincent.seguin.1@ulaval.ca>
2016-09-17 19:56:24 -0400

    ! Little fix in GB

--- java/com/personal/diecastfun/domain/Country.java
+++ java/com/personal/diecastfun/domain/Country.java
@@ -1,7 +1,7 @@
 package com.personal.diecastfun.domain;
 
 public enum Country {
-	Japan("jp"), UnitedStates("us"), Italy("it"), France("fr"), Germany("de"), UnitedKingdom("uk"), Romania(
+	Japan("jp"), UnitedStates("us"), Italy("it"), France("fr"), Germany("de"), UnitedKingdom("gb"), Romania(
 			"ro"), Spain("es"), SouthKorea("kr"), Australia("au"), Unknown("unknown"), Russia("ru"), Switzerland(
 					"ch"), Sweden("se"), CzechRepublic("cz"), China("cn"), Mexico("mx"), Austria("at"), Netherlands(
 							"nl");
