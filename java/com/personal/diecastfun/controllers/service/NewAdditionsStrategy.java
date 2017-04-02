dc005d0d4c177a1ead4395ec2b3ececd46f5365e
Vincent Séguin <vincent.seguin.1@ulaval.ca>
2017-04-02 14:24:09 -0400

    ! Dont cap for new additions

--- java/com/personal/diecastfun/controllers/service/NewAdditionsStrategy.java
+++ java/com/personal/diecastfun/controllers/service/NewAdditionsStrategy.java
@@ -10,7 +10,6 @@ import com.personal.diecastfun.domain.repositories.CarRepository;
 
 public class NewAdditionsStrategy extends Strategy
 {
-    private static final int NEW_ADDITIONS_SIZE = 15;
     private static final int NEW_ADDITIONS_DAYS = 15;
 
     private CarRepository carRepository;
@@ -26,6 +25,6 @@ public class NewAdditionsStrategy extends Strategy
         DateTime date = DateTime.now().minusDays(NEW_ADDITIONS_DAYS);
         List<Car> cars = carRepository.findByInsertionDateAfter(new Date(date.getMillis()));
 
-        return cars.subList(0, NEW_ADDITIONS_SIZE > cars.size() ? cars.size() : NEW_ADDITIONS_SIZE);
+        return cars;
     }
 }
